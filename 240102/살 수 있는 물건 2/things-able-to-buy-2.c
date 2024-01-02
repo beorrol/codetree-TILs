b7c3986aba3947b1d3a821b0263306603ba46dd5
JuHyun <68935085+beorrol@users.noreply.github.com>
2024-01-02 22:45:06 +0900

    Title: 살 수 있는 물건 2, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240102/살 수 있는 물건 2/things-able-to-buy-2.c
+++ 240102/살 수 있는 물건 2/things-able-to-buy-2.c
@@ -4,11 +4,11 @@ int main() {
     int a;
     scanf("%d", &a);
     if (a >= 3000) {
-        printf("3000");
+        printf("book");
     } else if (a >= 1000) {
-        printf("1000");
+        printf("mask");
     } else if (a >= 500) {
-        printf("500");
+        printf("pen");
     } else {
         printf("no");
     }
