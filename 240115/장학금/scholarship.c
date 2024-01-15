96dea2c25f09209a697ac09c00f0f97d1e895835
JuHyun <68935085+beorrol@users.noreply.github.com>
2024-01-15 12:46:33 +0900

    Title: 장학금, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240115/장학금/scholarship.c
+++ 240115/장학금/scholarship.c
@@ -3,7 +3,7 @@
 int main() {
     int a,b;
     if (a >= 90 && b >= 95) printf("100000");
-    else if (b >= 90) printf("50000");
+    else if (a >= 90 && b >= 90) printf("50000");
     else printf("0");
     return 0;
 }
